294fa3899d6931547eba377df3902e06f85b874b
gaetaj <gaetaj@oregonstate.edu>
2021-01-01 19:18:26 -0500

    added enough code to pass first test of valid pwd check

---
+++ check_pwd.py
@@ -0,0 +1,14 @@
+'''
+Author- Jacob Gaeta
+Test Driven Development Example
+
+Program that checks for valid passwords
+length between 8 and 20, one lowercase letter,
+one uppercase letter, one digit, one symbol.
+Returns true if requirements met, false otherwise
+'''
+
+
+def check_pwd(pwd):
+
+    return True
